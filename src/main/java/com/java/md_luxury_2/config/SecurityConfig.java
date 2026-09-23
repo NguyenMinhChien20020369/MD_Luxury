abe0ee6c9d6ea2b0c03d87ec9d917ab49ec39cef
ChienNM <chien9pm@gmail.com>
2026-09-23 16:32:46 +0700

    fix bean

--- src/main/java/com/java/md_luxury_2/config/SecurityConfig.java
+++ src/main/java/com/java/md_luxury_2/config/SecurityConfig.java
@@ -23,11 +23,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
--- src/test/java/com/java/md_luxury_2/MdLuxury2ApplicationTests.java
+++ src/test/java/com/java/md_luxury_2/MdLuxury2ApplicationTests.java
@@ -2,8 +2,10 @@ package com.java.md_luxury_2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MdLuxury2ApplicationTests {
 
     @Test
